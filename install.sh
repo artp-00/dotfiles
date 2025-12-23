2402a3dbe9b98cd12c248e5224979b1db8b54846
artp-00 <aptyp92160@gmail.com>
2025-12-23 15:01:08 +0100

    feat: install script v1

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
 read -p "This program will override your current configuration of the follwing programs:
-    - dunst
     - bashrc
     - zshrc
-    - ~/.scripts
+    - inputrc (requires sudo)
+    - i3
     - .vscode
     - picom
     - kitty
     - rofi
-    - i3
-are you sure you want to override it ? (Ctrl-C if no)" -n1 -s
-# rm -r "$HOME/.config/nvim"
+    - dunst
+    - ~/.scripts
+Are you sure you want to override it ? (Ctrl-C if no)" -n1 -s
 
 echo ""
 
-# ln -s "$(pwd)/nvim" ~/.config/
+rm ~/.zshrc;  ln -s "$(pwd)/.zshrc" ~/.zshrc
+rm ~/.bashrc;  ln -s "$(pwd)/.bashrc" ~/.bashrc
+sudo rm /etc/inputrc; sudo ln -s "$(pwd)/inputrc" /etc/inputrc
+rm ~/.config/i3/; ln -s "$(pwd)/i3" ~/.config/
+# TODO: vscode dotfiles
+rm -r ~/.config/picom/; ln -s "$(pwd)/picom" ~/.config/
+rm -r ~/.config/kitty/; ln -s "$(pwd)/kitty" ~/.config/
+rm -r ~/.config/rofi/; ln -s "$(pwd)/rofi" ~/.config/
+rm -r ~/.config/dunst/; ln -s "$(pwd)/dunst" ~/.config/
+# TODO: scripts
+
 
 echo "done"
 
